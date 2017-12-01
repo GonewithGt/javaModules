50d22182d9c85249f7893596dd05b77f4550c8ef
GonewithGt <1724532024@qq.com>
2017-12-01 16:23:21 +0800

    Update linuxCommand.sh

--- linuxCommand.sh
+++ linuxCommand.sh
@@ -2,6 +2,7 @@ free -m #查看内存使用情况
 echo 1 > /proc/sys/vm/drop_caches #清理内存 root权限
 grep "physical id" /proc/cpuinfo|sort -u|wc -l #查看cpu个数
 grep "cpu cores" /proc/cpuinfo|uniq #查看每个cpu内核个数
+lspci | grep -i vga #查看GPU使用情况
 cat /etc/passwd #用户名 用户密码 用户id 用户所在组id 备注 用户家目录 shell命令所在目录
 cat /etc/group #用户组 用户组口令 UID及该用户组包含的用户
 groups #查看当前登录用户的组内成员
